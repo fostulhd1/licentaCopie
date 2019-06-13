4f190f063e8d30adb7e42ae3841c812350d657e5
fostulhd1 <ciungradi.vlad@outlook.com>
2019-06-13 17:39:56 +0300

    First commit

---
+++ src/app/dashboard/dashboard.page.spec.ts
@@ -0,0 +1,31 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashboardPage } from './dashboard.page';
+
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser'
+
+/*describe('DashboardPage', () => {
+  let component: DashboardPage;
+  let fixture: ComponentFixture<DashboardPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DashboardPage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+*/
---
+++ src/app/dashboard/dashboard.page.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+
+import { NavController, ModalController } from '@ionic/angular';
+import { AuthenticationService } from '../services/authentication.service';
+
+import { CrudService } from '../services/crud.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+})
+export class DashboardPage implements OnInit {
+
+  userEmail: string;
+  students: any;
+  studentName: string;
+  studentAge: number;
+  studentAddress: string;
+
+  constructor(
+
+  	private navCtrl: NavController,
+    private authService: AuthenticationService,
+    private crudService: CrudService) { }
+
+  ngOnInit() {
+  
+  if(this.authService.userDetails()){
+      this.userEmail = this.authService.userDetails().email;
+    }else{
+      this.navCtrl.navigateBack('');
+    }
+
+     this.crudService.read_Students().subscribe(data => {
+ 
+      this.students = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          Name: e.payload.doc.data()['Name'],
+          Age: e.payload.doc.data()['Age'],
+          Address: e.payload.doc.data()['Address'],
+        };
+      })
+      console.log(this.students);
+ 
+    });
+  }
+ 
+  logout(){
+    this.authService.logoutUser()
+    .then(res => {
+      console.log(res);
+      this.navCtrl.navigateBack('');
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  CreateRecord() {
+    let record = {};
+    record['Name'] = this.studentName;
+    record['Age'] = this.studentAge;
+    record['Address'] = this.studentAddress;
+    this.crudService.create_NewStudent(record).then(resp => {
+		this.studentName = "";
+		this.studentAge = undefined;
+		this.studentAddress = "";
+		console.log(resp);
+    })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+   RemoveRecord(rowID) {
+    this.crudService.delete_Student(rowID);
+  }
+ 
+  EditRecord(record) {
+    record.isEdit = true;
+    record.EditName = record.Name;
+    record.EditAge = record.Age;
+    record.EditAddress = record.Address;
+  }
+ 
+  UpdateRecord(recordRow) {
+    let record = {};
+    record['Name'] = recordRow.EditName;
+    record['Age'] = recordRow.EditAge;
+    record['Address'] = recordRow.EditAddress;
+    this.crudService.update_Student(recordRow.id, record);
+    recordRow.isEdit = false;
+  }
+
+}
